6da34b43babb886d421b938adbae24ddc32c22e4
Cam Likes Kirby <92703353+CamLikesKirby@users.noreply.github.com>
2025-11-08 20:56:50 -0600

    Update ModHelperData and README.md

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,23 +6,7 @@ public static class ModHelperData
     public const string Version = "1.6.5";
     public const string Name = "Cams Pack!";
     public const string Dependencies = "doombubbles/paths-plus-plus";
-    public const string Description = """
-         (NOTICE: This mod may be split into differnt mods again, and the github will be archived soon, so watch out for that!)
-         This mod is pretty old so don't expect it to be good or balanced.
-
-         Everything I made in a pack!
-         Features:
-         New Towers,
-         New Paths, 
-         New Bloons, and
-         New Gamemodes 
-         (DM on discord if there's any problems with this mod Discord: camthekirby)
-
-         Credit: Nintendo (The Kirby characters), jacknjellify (Firey), u/PawaMV (The Dark Monkey Idea not the paragon), MaxLevelIdot (MaxLevelIdot)
-
-         I made everything else
-         """;
-
+    public const string Description = "(NOTICE: This mod may be split into differnt mods again, and the github will be archived soon, so watch out for that!)\n This mod is pretty old so don't expect it to be good or balanced. \nEverything I made in a pack! \nFeatures: \nNew Towers, \nNew Paths, \nNew Bloons, and \mNew Gamemodes \n(DM on discord if there's any problems with this mod Discord: camthekirby) \nCredit: Nintendo (The Kirby characters), jacknjellify (Firey), u/PawaMV (The Dark Monkey Idea not the paragon), MaxLevelIdot (MaxLevelIdot)\n I made everything else";
     public const string RepoOwner = "CamlikesKirby";
     public const string RepoName = "CamsPack";
 }
