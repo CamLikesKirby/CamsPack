58263382bbbccdffd3826afdb2d9a5bb2a3bc999
Cam Likes Kirby <92703353+CamLikesKirby@users.noreply.github.com>
2025-12-04 00:39:07 -0600

    1.5.8

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,8 +2,8 @@ namespace CamsPack;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "51";
-    public const string Version = "1.6.7";
+    public const string WorksOnVersion = "52";
+    public const string Version = "1.6.8";
     public const string Name = "Cams Pack!";
     public const string Dependencies = "doombubbles/paths-plus-plus";
     public const string Description = "(NOTICE: This mod will be split into differnt mods again, and the github will be archived soon, so watch out for that!)\n This mod is pretty old so don't expect it to be good or balanced.\nEverything I made in a pack!\nFeatures:\nNew Towers,\nNew Paths,\nNew Bloons, and\nNew Gamemodes\n(DM on discord if there's any problems with this mod Discord: camthekirby)\nCredit: Nintendo (The Kirby characters), jacknjellify (Firey), u/PawaMV (The Dark Monkey Idea not the paragon), MaxLevelIdot (MaxLevelIdot)\n I made everything else\n(Has ulimate crosspathing support)";
